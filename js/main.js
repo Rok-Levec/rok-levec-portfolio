2219aa04370e51655b43ddc2ef098c00e4871e3b
Levec Rok <rok.levec200@gmail.com>
2025-09-25 01:43:07 +0200

    Cleared up file

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
-fetch("navbar.html")
-  .then((res) => res.text())
-  .then((html) => {
-    document.getElementById("navbar").innerHTML = html;
-  });
-
 fetch("footer.html")
   .then((res) => res.text())
   .then((html) => {
     document.getElementById("footer").innerHTML = html;
   });
-
-// Modal functionality for enlarged project image
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.addEventListener("click", function (e) {
-    if (e.target.classList.contains("project-img")) {
-      const modal = document.getElementById("image-modal");
-      const modalImg = document.getElementById("modal-img");
-      modalImg.src = e.target.dataset.img;
-      modal.classList.remove("hidden");
-    }
-    if (
-      e.target.id === "close-modal" ||
-      (e.target.id === "image-modal" &&
-        e.target === document.getElementById("image-modal"))
-    ) {
-      document.getElementById("image-modal").classList.add("hidden");
-    }
-  });
-});
